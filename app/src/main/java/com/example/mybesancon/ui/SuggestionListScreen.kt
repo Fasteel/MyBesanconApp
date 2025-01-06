14dcf099b703be5ae66f7ec09ec1dd43d329e0de
Carl-MONNERA <cmonnera@regate.io>
2025-01-06 16:34:40 +0100

    feat: add tablet display

--- app/src/main/java/com/example/mybesancon/ui/SuggestionListScreen.kt
+++ app/src/main/java/com/example/mybesancon/ui/SuggestionListScreen.kt
@@ -2,15 +2,20 @@ package com.example.mybesancon.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,8 +24,11 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import androidx.window.core.layout.WindowWidthSizeClass
 import com.example.mybesancon.R
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import kotlin.math.absoluteValue
 
 @Composable
 fun SuggestionListScreen(
@@ -37,7 +45,7 @@ fun SuggestionListScreen(
             navBackStackEntry.arguments?.getString("categoryId")!!.toInt()
         }
     val suggestions =
-        uiState.collectAsState().value.suggestionDetail.filter { it.category.id == categoryId }
+        uiState.collectAsState().value.suggestionDetail.filter { it.category.id == categoryId.absoluteValue }
 
     if (suggestions.isEmpty()) {
         Column(
@@ -48,21 +56,74 @@ fun SuggestionListScreen(
             Spacer(modifier = Modifier.height(dimensionResource(R.dimen.big)))
             CircularProgressIndicator()
         }
+
+        return
     }
 
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(space = 12.dp),
-        modifier = Modifier.padding(dimensionResource(R.dimen.medium))
-    ) {
-        items(suggestions) {
-            SuggestionItem(
-                title = it.title,
-                mainPicture = it.mainPicture,
-                description = it.description,
-                onClickItem = { navController.navigate("${MyBesanconScreen.Detail.name}/${it.id}") }
-            )
+    val canShowSideSuggestionDetail =
+        currentWindowAdaptiveInfo().windowSizeClass.windowWidthSizeClass != WindowWidthSizeClass.COMPACT
+
+    if (canShowSideSuggestionDetail) {
+        val selectedSuggestionId =
+            rememberSaveable { mutableIntStateOf(uiState.value.suggestionDetail.first { categoryId == it.category.id }.id) }
+        // how to avoid collecting the state, is it possible?
+        val selectedSuggestion =
+            uiState.collectAsState().value.suggestionDetail.first { it.id == selectedSuggestionId.intValue }
+        val coroutineScope = rememberCoroutineScope()
+        val scrollState = rememberScrollState()
+
+        Row {
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(space = 12.dp),
+                modifier = Modifier
+                    .padding(dimensionResource(R.dimen.medium))
+                    .weight(1f)
+            ) {
+                items(suggestions) {
+                    SuggestionItem(
+                        title = it.title,
+                        mainPicture = it.mainPicture,
+                        description = it.description,
+                        onClickItem = {
+                            selectedSuggestionId.intValue = it.id
+                            coroutineScope.launch {
+                                scrollState.scrollTo(0)
+                            }
+                        }
+                    )
+                }
+            }
+            Column(
+                modifier = Modifier
+                    .padding(
+                        start = dimensionResource(R.dimen.medium),
+                        end = dimensionResource(R.dimen.medium)
+                    )
+                    .weight(1f)
+            ) {
+                SuggestionDetail(
+                    scrollState = scrollState,
+                    mainPicture = selectedSuggestion.mainPicture,
+                    content = selectedSuggestion.content
+                )
+            }
         }
-    }
+    } else {
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(space = 12.dp),
+            modifier = Modifier
+                .padding(dimensionResource(R.dimen.medium))
+        ) {
+            items(suggestions) {
+                SuggestionItem(
+                    title = it.title,
+                    mainPicture = it.mainPicture,
+                    description = it.description,
+                    onClickItem = { navController.navigate("${MyBesanconScreen.Detail.name}/${it.id}") }
+                )
+            }
+        }
 
+    }
 }
 
